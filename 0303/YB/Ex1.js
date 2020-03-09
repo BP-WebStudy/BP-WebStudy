b894ff48c3d6be29f604ad9a3276a454e1160938
luwbe1 <43837921+luwbe1@users.noreply.github.com>
2020-03-09 23:30:06 +0900

    Add files via upload

---
+++ 0303/YB/Ex1.js
@@ -0,0 +1,11 @@
+for (var word = '김유빈'; true;) {
+    var answer = prompt(word)
+    if (word[word.length - 1] === answer[0]) {
+        // 맞았을 때
+        alert('딩동댕')
+        word = answer;
+    } else {
+        // 틀렸을 때
+        alert('땡')
+    }
+}
